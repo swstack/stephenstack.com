434491fff635c9c9dba86b8d2007b43d6697a65f
Stephen Stack <stephen.stack@spectrumdsi.com>
2013-06-12 21:57:08 -0500

    example SqlAlchemy model using in-memory sqlite db.

---
+++ src/model/model.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from util.paths import PROJECT_ROOT
+
+
+class User(object):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+    def __init__(self, name, fullname, password):
+        self.name = name
+        self.fullname = fullname
+        self.password = password
+
+    def __repr__(self):
+       return "<User('%s','%s', '%s')>" % (self.name, self.fullname, self.password)
+
+
+if __name__ == "__main__":
+    db = create_engine('sqlite:///tutorial.db')
